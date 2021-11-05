bd6436029473583cc5dae5200ae3be52526e08c8
MulloaV <mulloa0179@e-camp.cl>
2021-11-05 20:13:21 -0300

    Maquetación lista, falta integrar el resto de la vista usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get 'dashboard/index'
   root to: 'home#index'
   namespace :admin do
     get 'home/index'
     get 'home/index' => 'home#index', :as => :root
+    
   end
 end
