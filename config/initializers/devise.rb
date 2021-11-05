9384de6d1d02f3398651a240db383293338ceaa1
MulloaV <mulloa0179@e-camp.cl>
2021-11-04 22:30:28 -0300

    Creado /users/dashboard/dashboarduser.html.erb, maqueteado tablas del dashboard, falta integrar vista de usuario y código RoR para mostrar los datos y los botones

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '9b2774a2036c086c4a76c4a1fdf0a3877cb70b89f22d10bfce2d9e4b895a559f24e9d50f48fbf2ac77ce23368827d6b1b328a7af045ef84d2615f4620a4e48c7'
+  # config.secret_key = '1247419276e24f5f42b68a8a9267d66f46477221fbc36aed876b79adc9c992d833d2fea0aa432d4148938ed94230436164964940f4ba1e030e634769e07d0ec9'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = 'df37d7f252725c9b440b192ac268e78f2134dd78200dc9cf48c96dcc5c4699187724b0d523b4ed5fd903a20ed7c1ec5634b297b0bb732952db4654243e12b6f2'
+  # config.pepper = '7aadcdff2b33bd7f3277801676111e9f261f0dc3ebfd089b7fbd23e494f0167446a824429c6d8347abfe056a46fc14f2e638a95341f07d7b9c4d0deb0d676bd2'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
